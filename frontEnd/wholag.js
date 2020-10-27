de2f1490bee175410fa7c76e107618a1caa16514
John Burns <johnb1003@gmail.com>
2020-10-27 14:24:41 -0400

    Refactor previous code -- Still incomplete

--- frontEnd/wholag.js
+++ frontEnd/wholag.js
@@ -1,35 +1,20 @@
-//let squad = ["Member 1", "Member 2", "Member 3", "Member 4"];
 let MAX_SQUAD = 4;
-memberStringNames = ['squad1', 'squad2', 'squad3', 'squad4']
+let memberStringNames = ['squad1', 'squad2', 'squad3', 'squad4']
 let squadNames = [];
-let picMem1 = 'https://vignette.wikia.nocookie.net/callofduty/images/0/06/Grinch_MW.PNG/revision/latest/scale-to-width-down/350?cb=20191101033554';
-let picMem2 = 'https://vignette.wikia.nocookie.net/callofduty/images/5/58/SimonRiley_Ghost_Season3_Intro_MW.png/revision/latest?cb=20200409114341'
-let picMem3 = 'https://vignette.wikia.nocookie.net/callofduty/images/f/f3/Alex_CampaignReveal_MW.jpg/revision/latest/scale-to-width-down/350?cb=20190925093902'
-let picMem4 = 'https://vignette.wikia.nocookie.net/callofduty/images/a/a4/KyleGarrick_MW.jpg/revision/latest/scale-to-width-down/350?cb=20190925112741'
+let picNames = ['member-1.png', 'member-2.png', 'member-3.jpg', 'member-4.jpg'];
 
-function getMember() {
-    if(squadNames.length < MAX_SQUAD) {
-        let input = document.getElementById('members');
-        squadNames.push(input.value);
-        updateMembersView();
-        //document.getElementById(memberStringNames[squadNames.length-1]).style.display = block;
-    }
-    else {
-        alert("Couldn't add member. Already at maximum squad capacity!");
-    }
-}
 
-function deleteMember(index) {
-    if(index >= 0 && index < squadNames.length) {
-        squadNames.splice(index, 1);
-        updateMembersView();
-        document.getElementById(memberStringNames[squadNames.length-1]).style.display = none;
-    }
-}
+$(document).ready( () => {
+    resizeBackdrop();
 
-function updateMembersView() {
-    for(i=0; i<squadNames.length; i++) {
-        document.getElementById(memberStringNames[i]).innerHTML = squadNames[i];
-    }
-    document.getElementById('squad1').innerHTML = squadNames[0];
+    $(window).resize( () => {
+        resizeBackdrop();
+    });
+});
+
+function resizeBackdrop() {
+    // Set bottom backdrop to cover full height of window
+    let backdropHeight = Math.max($('#cards-container').outerHeight(), 
+                    $(window).height() - ($('#heading').outerHeight() + $('#member-input-container').outerHeight()));
+    $('#backdrop').css('height', `${backdropHeight}px`);
 }
